3fd518be7375661272154eb53a77c84c8c4e5c52
way0utwest <sjones@sqlservercentral.com>
2023-10-23 15:53:36 -0600

    data type changes

--- schema-model/Tables/dbo.SizeTest.sql
+++ schema-model/Tables/dbo.SizeTest.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[SizeTest]
 (
 [myid] [int] NOT NULL,
-[mychar] [varchar] (20) NULL
+[mychar] [varchar] (20) NULL,
+[myint] [int] NULL
 )
 GO
 ALTER TABLE [dbo].[SizeTest] ADD CONSTRAINT [SizeTestPK] PRIMARY KEY CLUSTERED ([myid])
