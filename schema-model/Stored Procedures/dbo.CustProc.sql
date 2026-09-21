cd34a6bef8a33f3d39b367e6f6e9f07d5b4a07bb
way0utwest <sjones@sqlservercentral.com>
2026-09-21 08:26:13 -0600

    alter cust proc #3543 remove select *

--- schema-model/Stored Procedures/dbo.CustProc.sql
+++ schema-model/Stored Procedures/dbo.CustProc.sql
@@ -3,6 +3,14 @@ GO
 SET ANSI_NULLS ON
 GO
 CREATE PROCEDURE [dbo].[CustProc]
+/*
+2026-07-01 - removed * with explicit columns
+*/
 AS
-SELECT * FROM customer
+SELECT CustomerID,
+       CustomerName,
+       PrimaryContact,
+       PrimaryAddress,
+       PurchaseLimit,
+       Status FROM customer
 GO
