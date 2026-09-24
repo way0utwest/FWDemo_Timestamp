ea76dfcf02b184b27fa07ee6dc62ebb97bd17e49
way0utwest <sjones@sqlservercentral.com>
2026-09-24 15:21:24 -0600

    add customer filtering parameter to CustProc #3543

--- schema-model/Stored Procedures/dbo.CustProc.sql
+++ schema-model/Stored Procedures/dbo.CustProc.sql
@@ -3,13 +3,22 @@ GO
 SET ANSI_NULLS ON
 GO
 CREATE PROCEDURE [dbo].[CustProc]
+   @CustomerID INT = null
 /*
 2026-07-01 - removed * with explicit columns
 2026-07-01 - removed unneeded columns
+2026-08-12 - Added filtering
 */
 AS
+IF @CustomerID IS null
 SELECT CustomerID,
        CustomerName,
        PrimaryContact
        Status FROM customer
+else
+SELECT CustomerID,
+       CustomerName,
+       PrimaryContact
+       Status FROM customer
+       WHERE CustomerID = @CustomerID
 GO
